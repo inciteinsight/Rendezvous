ddb40324f20849763763138a741cb287b3b06541
AzuResolute <roger.palabasan@baruchmail.cuny.edu>
2019-10-10 15:53:37 -0400

    Backend Users Progress

---
+++ Application/User/Login.cs
@@ -0,0 +1,56 @@
+using System.Threading;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using FluentValidation;
+using MediatR;
+using Application.Errors;
+using Domain;
+
+namespace Application.User
+{
+    public class Login
+    {
+         public class Query : IRequest<AppUser>
+        {
+            public string Email { get; set; } 
+            public string Password { get; set; }
+        }
+        public class QueryValidator : AbstractValidator<Query>
+        {
+            public QueryValidator ()
+            {
+                RuleFor(x => x.Email).NotEmpty();
+                RuleFor(x => x.Password).NotEmpty();
+            }
+        }
+
+        public class Handler : IRequestHandler<Query, AppUser>
+        {
+            private readonly UserManager<AppUser> _userManager;
+            private readonly SignInManager<AppUser> _signInManager;
+            public Handler(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+            {
+                _userManager = userManager;
+                _signInManager = signInManager;
+            }
+            public async Task<AppUser> Handle(Query request, CancellationToken cancellationToken)
+            {
+                AppUser user = await _userManager.FindByEmailAsync(request.Email);
+
+                if (user == null)
+                {
+                    throw new RestException(HttpStatusCode.Unauthorized);
+                }
+                var result = await _signInManager.CheckPasswordSignInAsync(user, request.Password, false);
+
+                if (result.Succeeded)
+                {
+                    return user;
+                } else {
+                    throw new RestException(HttpStatusCode.Unauthorized);
+                }
+            }
+        }
+    }
+}
